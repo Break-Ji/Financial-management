21ebef03200cf05853fdff4cc85d2b2f14f8d485
Break.Ji <danchunxioaji@live.com>
2018-10-15 16:55:25 +0800

    financial 调整为多模块工程

--- financial/src/main/java/com/financial/Application.java
+++ financial/src/main/java/com/financial/Application.java
@@ -3,9 +3,11 @@ package com.financial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages = "com.financial.config")
+@ComponentScan()
 public class Application {
 
 	public static void main(String[] args) {
